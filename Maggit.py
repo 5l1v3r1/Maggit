f2efc562bc46a2e296dbbdfcccfa1db61059b2d1
Armando Vega <arm@ndov.net>
2017-05-27 23:13:19 +0200

    Some more comments translated to english

--- Maggit.py
+++ Maggit.py
@@ -26,21 +26,21 @@ import MaggitCmdUtils as mCmd
 
 
 if __name__ == '__main__':
-    # arparse koristimo za generiranje helpa i parsiranje argumenata
+    # use argparse for argument parsing
     parser = argparse.ArgumentParser(description='ICMP tunnel with remote command execution')
     parser.add_argument('-d', '--destination', help='Destination IP address', required=True)
     parser.add_argument('-s', '--source', help='Source IP address (Default: 0.0.0.0)', required=False, default='0.0.0.0')
     parser.add_argument('-e', '--encryption', help='Encryption algorithm (Default: AES)', required=False, choices=['AES','ROT13', 'PLAIN'],  default='AES')
     parser.add_argument('-k', '--key', help='Encryption key/password (Default: "thisisapassword")', required=False, default='thisisapassword')
     
-    # ako nema argumenata ispisi help
+    # if there are no arguments, print out help / usage
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(0)
     else:
         params = parser.parse_args()
     
-    # prebacivanje teksta u numerice konstante
+    # set up the algo to be used
     if params.encryption == 'AES':
         algo = mData.ALGO_AES
     elif params.encryption == 'ROT13':
@@ -48,13 +48,13 @@ if __name__ == '__main__':
     else:
         algo = mData.ALGO_PLAIN
     
-    # startamo novi kanal
+    # start a new channel
     chan = mNet.ICMPChannel(src_ip_addr = params.source, dst_ip_addr=params.destination, algo=algo, password=params.key)
     chan.start()
     
-    # predajemo kontrolu nad programom interfejsu
+    # hand over control to the interface module
     mCmd.CommandInterface(chan)
     
-    # zatvaramo kanal
+    # close the channel
     chan.close()
 
